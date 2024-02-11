48a55c8f18b8ff8c9d20e542c5ae30fd9bcabdc6
Thiago <Thiago@Lenovo>
2024-02-11 12:48:05 -0300

    Codigos Javas

---
+++ src/ProgII_Grafica_BancoDado/ProgII_Grafica_BancoDados.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ProgII_Grafica_BancoDado;
+
+/**
+ *
+ * @author Thiago
+ */
+public class ProgII_Grafica_BancoDados {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("pla");
+    }
+    
+}
